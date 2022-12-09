e345ae80a6ab0df79e95ca26199e3cb926f13930
ColeKatz1 <84477747+ColeKatz1@users.noreply.github.com>
2022-12-09 00:24:17 -0500

    Update Player_Data_Scraper.py

--- Player_Data_Scraper.py
+++ Player_Data_Scraper.py
@@ -25,7 +25,7 @@ def findTables(url):
 
 def pullTable(url, tableID):
     res = requests.get(url)
-    sleep(randint(1,5))
+    sleep(randint(5,8))
     comm = re.compile("<!--|-->")
     soup = bs4.BeautifulSoup(comm.sub("", res.text), 'lxml')
     tables = soup.findAll('table', id = tableID)
@@ -45,7 +45,7 @@ def pullTable(url, tableID):
     data = data.reset_index(drop = True)
     return(data)
 
-def boxScoreUrls(url):
+def boxScoreUrls(url): #how to fix box scores issues with last game being included for some, you just search the html for last game. If it is found, you take [2:]. If not, you take [1:]
     res = requests.get(url, headers = {'User-agent': 'youaojsdoiajsdoijsado'})
     comm = re.compile("<!--|-->")
     soup = bs4.BeautifulSoup(comm.sub("", res.text), 'lxml')
@@ -57,12 +57,45 @@ def boxScoreUrls(url):
     r = re.compile("/boxes/\w\w\w/\w\w\w\d")  
     specificLink = list(filter(r.match, dataLink))
     dfOfLinks = pd.DataFrame(specificLink, columns=['url'])
-    dfOfLinks = dfOfLinks.drop_duplicates()
+    #dfOfLinks = dfOfLinks.drop_duplicates()
     urlList = dfOfLinks.values.tolist()
     finalUrlList = ['https://www.baseball-reference.com' + str(i) for i in urlList]
     finalUrlList = [i.replace('[\'','') for i in finalUrlList]
     finalUrlList = [i.replace('\']','') for i in finalUrlList]
     return(finalUrlList)
+
+
+def boxScoreUrlsPlayerData(url): #how to fix box scores issues with last game being included for some, you just search the html for last game. If it is found, you take [2:]. If not, you take [1:]
+    res = requests.get(url, headers = {'User-agent': 'youaojsdoiajsdoijsado'})
+    comm = re.compile("<!--|-->")
+    soup = bs4.BeautifulSoup(comm.sub("", res.text), 'lxml')
+    strong = soup.findAll('strong')
+    booleanLastGame = False
+    for s in strong:
+        if str(s) == "<strong>Last Game:</strong>":
+            booleanLastGame = True
+            break
+        else:
+            booleanLastGame = False
+    dataLink = []
+    for link in soup.findAll('a'):
+        if link.has_attr('href'):
+            allLinks = link.attrs['href']
+            dataLink.append(allLinks)
+    r = re.compile("/boxes/\w\w\w/\w\w\w\d")  
+    specificLink = list(filter(r.match, dataLink))
+    dfOfLinks = pd.DataFrame(specificLink, columns=['url'])
+    #dfOfLinks = dfOfLinks.drop_duplicates()
+    urlList = dfOfLinks.values.tolist()
+    finalUrlList = ['https://www.baseball-reference.com' + str(i) for i in urlList]
+    finalUrlList = [i.replace('[\'','') for i in finalUrlList]
+    finalUrlList = [i.replace('\']','') for i in finalUrlList]
+    if booleanLastGame == True:
+        finalUrlList = finalUrlList[2:]
+    else:
+        finalUrlList = finalUrlList[1:]
+    return(finalUrlList)
+
 
 def getStartingLineupInfo(boxScoreUrl, teamAbbreviation):
     sleep(randint(5,10))
@@ -113,11 +146,11 @@ def getListOfAllStarters(scheduleUrl, teamAbbreviation):
     listOfBatters = []
     listOfPitchers = []
     urls = boxScoreUrls(scheduleUrl) #https://www.baseball-reference.com/boxes/COL/COL202207280.shtml this one doesn't have a pitcher
-    #urls = urls[:10]
     for url in urls:
         print(url)
-        startingBatters = getStartingLineupInfo(url, teamAbbreviation)[0]['newPlayerName'].values.tolist()
-        startingPitcher = getStartingLineupInfo(url, teamAbbreviation)[1]['newPlayerName'].values.tolist()
+        allStarters = getStartingLineupInfo(url, teamAbbreviation)
+        startingBatters = allStarters[0]['newPlayerName'].values.tolist()
+        startingPitcher = allStarters[1]['newPlayerName'].values.tolist()
         listOfBatters.append(startingBatters)
         listOfPitchers.append(startingPitcher)
     for x in listOfBatters:
@@ -152,25 +185,14 @@ def uploadStarterList(scheduleUrl, teamAbbreviation, year):
     credentials.upload_file(Filename = year + '_' + teamAbbreviation + '_Batters.csv', Bucket = 'mlbplayerdata', Key = year + '_' + teamAbbreviation + '_Batters.csv')
     credentials.upload_file(Filename = year + '_' + teamAbbreviation + '_Pitchers.csv', Bucket = 'mlbplayerdata', Key = year + '_' + teamAbbreviation + '_Pitchers.csv')
 
-
-def getPlayerStatsLink(boxScoreUrl, year): #just get the batting data for every 
-    batterGameLogLinks = []
-    pitcherGameLogLinks = []
-    df = getStartingLineupInfo(boxScoreUrl)
-    for i in range(len(df['newPlayerName'])): 
-        if df['battingOrder'][i] == '20' and df['playerPosition'][i] == 'P':
-            pitcherGameLogLinks.append("https://www.baseball-reference.com/players/gl.fcgi?id=" + df['newPlayerName'][i] + "01&t=p" + "&year=" + year)
-        else:
-            batterGameLogLinks.append("https://www.baseball-reference.com/players/gl.fcgi?id=" + df['newPlayerName'][i] + "01&t=b" + "&year=" + year)
-    return pitcherGameLogLinks, batterGameLogLinks
-
 def pullPitcherData(playerLink):
-    sleep(randint(4,8))
     df = pullTable(playerLink,"pitching_gamelogs")
-    boxScores = boxScoreUrls(playerLink)
-    boxScores = boxScores[1:] #one thing you can consider is shifting this by 1 so that you have a new variable "boxScoreOfNextGame" then you just use that to combine
+    boxScores = boxScoreUrlsPlayerData(playerLink)
     df = df[df.ERA != "Pit"]
     df.drop(df.tail(1).index,inplace=True)
+    pattern = "Play\w+"
+    filter = df['Tm'].str.contains(pattern)
+    df = df[~filter]
     df = df.reset_index()
     df['boxScores'] = boxScores
     df = df.drop(['index'],axis=1)
@@ -178,10 +200,12 @@ def pullPitcherData(playerLink):
 
 def pullBatterData(playerLink):
     df = pullTable(playerLink,"batting_gamelogs")
-    boxScores = boxScoreUrls(playerLink)
-    boxScores = boxScores[1:] #one thing you can consider is shifting this by 1 so that you have a new variable "boxScoreOfNextGame" then you just use that to combine
+    boxScores = boxScoreUrlsPlayerData(playerLink)
     df = df[df.Opp != "Rslt"]
-    df.drop(df.tail(1).index,inplace=True)
+    df.drop(df.tail(1).index,inplace=True) #need to account for trades, so also drop 
+    pattern = "Play\w+"
+    filter = df['Tm'].str.contains(pattern)
+    df = df[~filter]
     df = df.reset_index()
     df['boxScores'] = boxScores
     df = df.drop(['index'],axis=1)
@@ -204,20 +228,32 @@ def getStartingPlayerLinks(teamAbbreviation, year):
 
 def uploadStartingPitcherData(teamAbbreviation, year):
     links = getStartingPlayerLinks(teamAbbreviation, year)[1]
-    links = links[:3]
     credentials = boto3.client('s3', aws_access_key_id = access_key, 
                           aws_secret_access_key= secret_access_key) 
     for link in links:
         playerName = re.findall("id=\w+&",link)
         playerName = str(playerName)
         playerName= playerName[5:-3]
+        print(link)
         data = pullPitcherData(link)
         data.to_csv(year + '_' + playerName + '_Pitching_Logs.csv')
         credentials.upload_file(Filename = year + '_' + playerName + '_Pitching_Logs.csv', Bucket = 'mlbplayerdata', Key = year + '_' + playerName + '_Pitching_Logs.csv')
 
 
-#print(getStartingPlayerLinks("ARI","2021")[1])
-uploadStartingPitcherData("ARI","2021")
-#uploadStarterList("https://www.baseball-reference.com/teams/ARI/2021-schedule-scores.shtml","ARI", "2021")
-#request = requests.get("https://www.baseball-reference.com/teams/LAD/2022-schedule-scores.shtml")
-#print(request)  
+def uploadStartingBatterData(teamAbbreviation, year):
+    links = getStartingPlayerLinks(teamAbbreviation, year)[0]
+    credentials = boto3.client('s3', aws_access_key_id = access_key, 
+                          aws_secret_access_key= secret_access_key) 
+    for link in links:
+        playerName = re.findall("id=\w+&",link)
+        playerName = str(playerName)
+        playerName= playerName[5:-3]
+        print(link)
+        data = pullBatterData(link)
+        data.to_csv(year + '_' + playerName + '_Batting_Logs.csv')
+        credentials.upload_file(Filename = year + '_' + playerName + '_Batting_Logs.csv', Bucket = 'mlbplayerdata', Key = year + '_' + playerName + '_Batting_Logs.csv')
+
+
+uploadStartingPitcherData("ATL","2021")
+
+#pullTable("https://www.baseball-reference.com/players/gl.fcgi?id=youngal01&t=p&year=2021",)
